889a4d2c6e36196999d3fc3b694a2ea3ee8d9b1f
Haidar0096 <haidarrmehsen@gmail.com>
2022-06-23 14:54:00 +0300

    added tests and refactored code of screenshot service

--- lib/services/screenshot_service/screenshot_service.dart
+++ lib/services/screenshot_service/screenshot_service.dart
@@ -1,9 +1,7 @@
 import 'dart:typed_data';
 
-import 'package:dartz/dartz.dart';
 import 'package:flutter/widgets.dart' as widgets;
 import 'package:injectable/injectable.dart';
-import 'package:photo_editor/common/failure/failure.dart';
 import 'package:photo_editor/services/screenshot_service/screenshot_service_impl.dart';
 
 /// A service that takes a screenshot of a specific widget or screen.
@@ -14,26 +12,9 @@ abstract class ScreenshotService {
 
   /// Captures the screenshot of the given widget and returns it as a [Uint8List].
   ///
-  /// If an error occurs, it will return a [CaptureWidgetFailure].
-  ///
   /// The [BuildContext] - if provided - is used to find the theme of the widget.
-  Future<Either<Failure, Uint8List>> captureWidget({
+  Future<Uint8List> captureWidget({
     widgets.BuildContext? context,
     required widgets.Widget widget,
   });
-}
-
-/// The failure that occurs when the screenshot service fails to capture a widget.
-class CaptureWidgetFailure extends Failure {
-  /// The exception that was thrown when the screenshot service failed to capture a widget.
-  final Exception exception;
-
-  CaptureWidgetFailure(this.exception);
-
-  @override
-  String get message =>
-      'Could not capture the widget. The exception was: $exception';
-
-  @override
-  String get code => 'capture_widget_failure';
 }
--- lib/services/screenshot_service/screenshot_service_impl.dart
+++ lib/services/screenshot_service/screenshot_service_impl.dart
@@ -1,35 +1,28 @@
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
-import 'package:dartz/dartz.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart' as widgets;
-import 'package:photo_editor/common/failure/failure.dart';
 import 'package:photo_editor/extensions/double_extension.dart';
 import 'package:photo_editor/services/screenshot_service/screenshot_service.dart';
 
 /// The default implementation of ScreenshotService. It uses a 3rd party package to implement the contract.
 class ScreenshotServiceDefaultImpl implements ScreenshotService {
   @override
-  Future<Either<Failure, Uint8List>> captureWidget({
+  Future<Uint8List> captureWidget({
     widgets.BuildContext? context,
     required widgets.Widget widget,
   }) async {
-    try {
-      final Uint8List data = await _captureFromWidget(
-        widget,
-        context: context,
-      );
-      return right(data);
-    } on Exception catch (e) {
-      return Left(CaptureWidgetFailure(e));
-    }
+    return await _captureFromWidget(
+      widget,
+      context: context,
+    );
   }
 
   /// [context] parameter is used to Inherit App Theme and MediaQuery data.
   Future<Uint8List> _captureFromWidget(
     widgets.Widget widget, {
-    // Duration delay = const Duration(seconds: 1),
+    Duration delay = const Duration(seconds: 1),
     double? pixelRatio,
     widgets.BuildContext? context,
   }) async {
@@ -106,8 +99,8 @@ class ScreenshotServiceDefaultImpl implements ScreenshotService {
       image = await repaintBoundary.toImage(
           pixelRatio: pixelRatio ?? (imageSize.width / logicalSize.width));
 
-      // // This delay should increase with Widget tree Size
-      // await Future.delayed(delay);
+      // This delay should increase with Widget tree Size
+      await Future.delayed(delay);
 
       // Check does this require rebuild
       if (isDirty) {
