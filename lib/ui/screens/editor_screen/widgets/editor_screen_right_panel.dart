411649ea8d4060ab613b8f52f7670abe68d3c99a
Haidar0096 <haidarrmehsen@gmail.com>
2022-08-01 16:56:50 +0300

    code cleanup in editor_screen_right_panel.dart

--- lib/ui/screens/editor_screen/widgets/editor_screen_right_panel.dart
+++ lib/ui/screens/editor_screen/widgets/editor_screen_right_panel.dart
@@ -13,51 +13,50 @@ class EditorScreenRightPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData toc = Theme.of(context);
     return BlocBuilder<EditorBloc, EditorState>(
-      builder: (context, editorState) {
-        // TODO: Add options to style the variable and static text, and change the source file of variable text.
-        // TODO: then add option to process the data (generate the images with the variables set) and proper error handling
-        // TODO: then add option to save the currently edited widget locally or online
-        // TODO: remove the widgets below in favor of the widgets mentioned above
-
-        return Container(
-          color: toc.colorScheme.primary,
-          child: editorState.selectedElementId
-              .flatMap<Widget>(
-                (id) => editorState.editor.elementById(id).map(
-                      (el) => ListView(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(AppLocalizations.of(context)!.properties, textAlign: TextAlign.center),
-                          ),
-                          Divider(color: toc.colorScheme.onSecondary),
-                          _xPositionText(el, toc, editorState, context),
-                          Divider(color: toc.colorScheme.onSecondary),
-                          _yPositionText(el, toc, editorState, context),
-                          Divider(color: toc.colorScheme.onSecondary),
-                          _elementWidthText(el, toc, editorState, context),
-                          Divider(color: toc.colorScheme.onSecondary),
-                          _elementHeightText(el, toc, editorState, context),
-                          Divider(color: toc.colorScheme.onSecondary),
-                          // if selected element is text element:
-                          if (el.properties.isStaticTextProperties || el.properties.isVariableTextProperties) ...[
-                            _elementSizeText(el, toc, editorState, context),
-                            Divider(color: toc.colorScheme.onSecondary),
-                          ],
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('Show Order: ${el.showOrder}', textAlign: TextAlign.center),
-                          ),
-                          Divider(color: toc.colorScheme.onSecondary),
+      // TODO: Add options to style the variable and static text, and change the source file of variable text.
+      // TODO: then add option to process the data (generate the images with the variables set) and proper error handling
+      // TODO: then add option to save the currently edited widget locally or online
+      // TODO: remove the widgets below in favor of the widgets mentioned above
+      builder: (context, editorState) => Container(
+        color: toc.colorScheme.primary,
+        child: editorState.selectedElementId
+            .flatMap<Widget>(
+              (id) => editorState.editor.elementById(id).map(
+                    (el) => ListView(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(AppLocalizations.of(context)!.properties, textAlign: TextAlign.center),
+                        ),
+                        _divider(context),
+                        _xPositionText(context, el),
+                        _divider(context),
+                        _yPositionText(context, el),
+                        _divider(context),
+                        _elementWidthText(context, el),
+                        _divider(context),
+                        _elementHeightText(context, el),
+                        _divider(context),
+                        // if selected element is text element:
+                        if (el.properties.isStaticTextProperties || el.properties.isVariableTextProperties) ...[
+                          _elementSizeText(context, el),
+                          _divider(context),
                         ],
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Show Order: ${el.showOrder}', textAlign: TextAlign.center),
+                        ),
+                        _divider(context),
+                      ],
                     ),
-              )
-              .getOrElse(() => Container()),
-        );
-      },
+                  ),
+            )
+            .getOrElse(() => Container()),
+      ),
     );
   }
+
+  Divider _divider(BuildContext context) => Divider(color: Theme.of(context).colorScheme.onSecondary);
 
   Widget _createDoubleStateText({
     required BuildContext context,
@@ -85,77 +84,61 @@ class EditorScreenRightPanel extends StatelessWidget {
     );
   }
 
-  Widget _xPositionText(Element el, ThemeData toc, EditorState editorState, BuildContext context) {
-    late final Offset currentOffset;
-    editorState.selectedElementId
-        .map((id) => editorState.editor.elementById(id).map((el) => currentOffset = el.rect.topLeft));
+  Widget _xPositionText(BuildContext context, Element el) => _createDoubleStateText(
+        context: context,
+        initialText: 'x: ${el.rect.topLeft.dx.toStringAsFixed(2)}',
+        onGainFocus: () => el.rect.topLeft.dx.toStringAsFixed(2),
+        onLoseFocus: (updatedValue) {
+          double? updatedX = double.tryParse(updatedValue) ?? el.rect.topLeft.dx;
+          context
+              .read<EditorBloc>()
+              .add(EditorEvent.selectedElementPositionChanged(Offset(updatedX, el.rect.topLeft.dy)));
+          return 'x: ${el.rect.topLeft.dx.toStringAsFixed(2)}';
+        },
+      );
 
-    return _createDoubleStateText(
-      context: context,
-      initialText: 'x: ${el.rect.topLeft.dx.toStringAsFixed(2)}',
-      onGainFocus: () => currentOffset.dx.toStringAsFixed(2),
-      onLoseFocus: (updatedValue) {
-        double? updatedX = double.tryParse(updatedValue) ?? currentOffset.dx;
-        context.read<EditorBloc>().add(EditorEvent.selectedElementPositionChanged(Offset(updatedX, currentOffset.dy)));
-        return 'x: ${el.rect.topLeft.dx.toStringAsFixed(2)}';
-      },
-    );
-  }
+  Widget _yPositionText(BuildContext context, Element el) => _createDoubleStateText(
+        context: context,
+        initialText: 'y: ${el.rect.topLeft.dy.toStringAsFixed(2)}',
+        onGainFocus: () => el.rect.topLeft.dy.toStringAsFixed(2),
+        onLoseFocus: (updatedValue) {
+          double? updatedY = double.tryParse(updatedValue) ?? el.rect.topLeft.dy;
+          context
+              .read<EditorBloc>()
+              .add(EditorEvent.selectedElementPositionChanged(Offset(el.rect.topLeft.dx, updatedY)));
+          return 'y: ${el.rect.topLeft.dy.toStringAsFixed(2)}';
+        },
+      );
 
-  Widget _yPositionText(Element el, ThemeData toc, EditorState editorState, BuildContext context) {
-    late final Offset currentOffset;
-    editorState.selectedElementId
-        .map((id) => editorState.editor.elementById(id).map((el) => currentOffset = el.rect.topLeft));
+  Widget _elementWidthText(BuildContext context, Element el) => _createDoubleStateText(
+        context: context,
+        initialText: 'w: ${el.rect.size.width.toStringAsFixed(2)}',
+        onGainFocus: () => el.rect.size.width.toStringAsFixed(2),
+        onLoseFocus: (updatedValue) {
+          double updatedWidth = double.tryParse(updatedValue) ?? -1;
 
-    return _createDoubleStateText(
-      context: context,
-      initialText: 'y: ${el.rect.topLeft.dy.toStringAsFixed(2)}',
-      onGainFocus: () => currentOffset.dy.toStringAsFixed(2),
-      onLoseFocus: (updatedValue) {
-        double? updatedY = double.tryParse(updatedValue) ?? currentOffset.dy;
-        context.read<EditorBloc>().add(EditorEvent.selectedElementPositionChanged(Offset(currentOffset.dx, updatedY)));
-        return 'y: ${el.rect.topLeft.dy.toStringAsFixed(2)}';
-      },
-    );
-  }
+          context
+              .read<EditorBloc>()
+              .add(EditorEvent.selectedElementSizeChanged(Size(updatedWidth, el.rect.size.height)));
+          return 'w: ${el.rect.size.width.toStringAsFixed(2)}';
+        },
+      );
 
-  Widget _elementWidthText(Element el, ThemeData toc, EditorState editorState, BuildContext context) {
-    late final Size currentSize;
-    editorState.selectedElementId
-        .map((id) => editorState.editor.elementById(id).map((el) => currentSize = el.rect.size));
+  Widget _elementHeightText(BuildContext context, Element el) => _createDoubleStateText(
+        context: context,
+        initialText: 'h: ${el.rect.size.height.toStringAsFixed(2)}',
+        onGainFocus: () => el.rect.size.height.toStringAsFixed(2),
+        onLoseFocus: (updatedValue) {
+          double updatedHeight = double.tryParse(updatedValue) ?? -1;
 
-    return _createDoubleStateText(
-      context: context,
-      initialText: 'w: ${el.rect.size.width.toStringAsFixed(2)}',
-      onGainFocus: () => currentSize.width.toStringAsFixed(2),
-      onLoseFocus: (updatedValue) {
-        double updatedWidth = double.tryParse(updatedValue) ?? -1;
+          context
+              .read<EditorBloc>()
+              .add(EditorEvent.selectedElementSizeChanged(Size(el.rect.size.width, updatedHeight)));
+          return 'h: ${el.rect.height.toStringAsFixed(2)}';
+        },
+      );
 
-        context.read<EditorBloc>().add(EditorEvent.selectedElementSizeChanged(Size(updatedWidth, currentSize.height)));
-        return 'w: ${el.rect.size.width.toStringAsFixed(2)}';
-      },
-    );
-  }
-
-  Widget _elementHeightText(Element el, ThemeData toc, EditorState editorState, BuildContext context) {
-    late final Size currentSize;
-    editorState.selectedElementId
-        .map((id) => editorState.editor.elementById(id).map((el) => currentSize = el.rect.size));
-    return _createDoubleStateText(
-      context: context,
-      initialText: 'h: ${el.rect.size.height.toStringAsFixed(2)}',
-      onGainFocus: () => currentSize.height.toStringAsFixed(2),
-      onLoseFocus: (updatedValue) {
-        double updatedHeight = double.tryParse(updatedValue) ?? -1;
-
-        context.read<EditorBloc>().add(EditorEvent.selectedElementSizeChanged(Size(currentSize.width, updatedHeight)));
-        return 'h: ${el.rect.height.toStringAsFixed(2)}';
-      },
-    );
-  }
-
-  Widget _elementSizeText(Element el, ThemeData toc, EditorState editorState, BuildContext context) {
-    // check if the selected element is text, and get its relevant properties to be used by the button handlers:
+  Widget _elementSizeText(BuildContext context, Element el) {
     TextStyle? currentStyle;
 
     if (el.properties.isStaticTextProperties) {
