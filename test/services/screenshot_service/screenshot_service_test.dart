4c14b660330b759a311267d5b5775afb598b6835
Haidar0096 <haidarrmehsen@gmail.com>
2022-09-15 15:58:46 +0300

    refactored screenshot_service_test.dart so that it is not platform dependent now

--- test/services/screenshot_service/screenshot_service_test.dart
+++ test/services/screenshot_service/screenshot_service_test.dart
@@ -1,14 +1,16 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:photo_editor/services/screenshot_service/screenshot_service.dart';
 
-part 'screenshot_service_test_constants.dart'; // contains the bytes of the captured image and the app widget
 
 void main() {
   group('captureWidget', () {
-    testWidgets('Should return the correct Uint8List when successful.', (WidgetTester tester) async {
+    testWidgets('Should return the correct Uint8List when successful.',
+        (WidgetTester tester) async {
       TestWidgetsFlutterBinding.ensureInitialized();
 
       // setup the physical size
@@ -19,6 +21,18 @@ void main() {
       addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
       addTearDown(tester.binding.window.clearDevicePixelRatioTestValue);
 
+
+      final Widget appWidget = MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      );
       await tester.pumpWidget(appWidget);
       await tester.pumpAndSettle();
 
@@ -26,20 +40,23 @@ void main() {
       ScreenshotService screenshotService = ScreenshotService();
 
       // capture the widget and get the actual result Uint8List
-      final List<int>? actualResult =
-          await tester.runAsync<List<int>?>(() async => (await screenshotService.captureWidget(
+      final Uint8List? actualResult = await tester.runAsync<Uint8List?>(
+          () async => (await screenshotService.captureWidget(
                 widget: appWidget,
-                context: tester.element(find.byType(Text)),
+                context: tester.element(find.byType(Container)),
                 outputImageSize: const Size(1366, 768),
                 window: tester.binding.window,
                 delay: const Duration(milliseconds: 1000),
                 outputImagePixelRatio: 1.0,
                 outputImageByteFormat: ImageByteFormat.png,
-              ))
-                  .toList());
+              )));
+
+      // file that contains an expected screenshot of the widget
+      final File expectedResultFile = File(
+          'test_resources/screenshot_service_test_resources/capture_widget/test_image.png');
 
       // assert the result is correct
-      expect(actualResult, expectedResult);
+      expect(actualResult, expectedResultFile.readAsBytesSync());
     });
   });
 }
